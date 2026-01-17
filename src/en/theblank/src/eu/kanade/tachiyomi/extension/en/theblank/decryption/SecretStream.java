3e6c19fc88ff0455c363970bdfb1c523fd164730
Clouddark75 <84534855+Clouddark75@users.noreply.github.com>
2026-01-17 03:57:45 -0400

    Update SecretStream.java

--- src/en/theblank/src/eu/kanade/tachiyomi/extension/en/theblank/decryption/SecretStream.java
+++ src/en/theblank/src/eu/kanade/tachiyomi/extension/en/theblank/decryption/SecretStream.java
@@ -1,11 +1,3 @@
-/*
- * crypto_secretstream_xchacha20poly1305
- * Port directo y funcional para Tachiyomi / Mihon
- *
- * Basado en libsodium
- * https://github.com/jedisct1/libsodium
- */
-
 package eu.kanade.tachiyomi.extension.en.theblank.decryption;
 
 import java.security.MessageDigest;
@@ -13,78 +5,89 @@ import java.util.Arrays;
 
 public final class SecretStream {
 
+    public static final int TAG_MESSAGE = 0;
+    public static final int TAG_FINAL = 3;
+
     private static final int KEYBYTES = 32;
     private static final int NONCEBYTES = 24;
-    private static final int SUBKEYBYTES = 32;
     private static final int TAGBYTES = 16;
-    private static final int HEADERBYTES = 24;
 
-    private SecretStream() {}
+    public static final class Result {
+        public final byte[] message;
+        public final byte tag;
+
+        Result(byte[] message, byte tag) {
+            this.message = message;
+            this.tag = tag;
+        }
+    }
+
+    public SecretStream() {
+    }
 
     /* =========================
-     * INIT (cliente)
+     * INIT
      * ========================= */
-    public static byte[] initPull(State state, byte[] header, byte[] key) {
-        if (key.length != KEYBYTES || header.length != HEADERBYTES) {
-            throw new IllegalArgumentException("Invalid key or header size");
+    public int initPull(State state, byte[] header, byte[] key) {
+        if (key.length != KEYBYTES || header.length != NONCEBYTES) {
+            return -1;
         }
 
-        byte[] subKey = new byte[SUBKEYBYTES];
-
-        // Derivar subkey con HChaCha20
+        byte[] subKey = new byte[32];
         Core.hChaCha20(subKey, header, key);
 
-        // Copiar nonce (últimos 8 bytes = contador)
+        Arrays.fill(state.nonce, (byte) 0);
         System.arraycopy(header, 16, state.nonce, 4, 8);
-        Arrays.fill(state.nonce, 0, 4, (byte) 0);
 
         System.arraycopy(subKey, 0, state.k, 0, 32);
         Arrays.fill(subKey, (byte) 0);
 
-        return header;
+        return 0;
     }
 
     /* =========================
      * DECRYPT
      * ========================= */
-    public static byte[] pull(State state, byte[] cipher) throws Exception {
-        if (cipher.length < TAGBYTES) {
-            throw new IllegalArgumentException("Ciphertext too short");
+    public Result pull(State state, byte[] cipher, int cipherLen) throws Exception {
+        if (cipherLen < TAGBYTES) {
+            return null;
         }
 
-        int mlen = cipher.length - TAGBYTES;
-        byte[] message = new byte[mlen];
-        byte[] tag = new byte[TAGBYTES];
+        int msgLen = cipherLen - TAGBYTES;
+        byte[] msg = new byte[msgLen];
+        byte[] mac = new byte[TAGBYTES];
 
-        System.arraycopy(cipher, mlen, tag, 0, TAGBYTES);
+        System.arraycopy(cipher, msgLen, mac, 0, TAGBYTES);
 
-        // Generar Poly1305 key
+        // Poly1305 key
         byte[] polyKey = new byte[32];
         ChaCha20.xor(polyKey, polyKey, 32, state.nonce, state.k);
 
-        // Verificar MAC
         Poly1305 poly = new Poly1305(polyKey);
-        poly.update(cipher, 0, mlen);
-        poly.update(new byte[16], 0, (16 - (mlen % 16)) % 16);
+        poly.update(cipher, 0, msgLen);
+        poly.update(new byte[16], 0, (16 - msgLen % 16) % 16);
         poly.update(new byte[8], 0, 8);
-        poly.update(longToBytes(mlen), 0, 8);
+        poly.update(longToBytes(msgLen), 0, 8);
 
-        byte[] computedTag = poly.finish();
+        byte[] computed = poly.finish();
 
-        if (!MessageDigest.isEqual(tag, computedTag)) {
+        if (!MessageDigest.isEqual(mac, computed)) {
             throw new SecurityException("Invalid MAC");
         }
 
-        // Decrypt
-        ChaCha20.xor(message, cipher, mlen, state.nonce, state.k);
-
+        ChaCha20.xor(msg, cipher, msgLen, state.nonce, state.k);
         incrementNonce(state.nonce);
 
-        return message;
+        byte tag = TAG_MESSAGE;
+        if (msgLen == 0) {
+            tag = TAG_FINAL;
+        }
+
+        return new Result(msg, tag);
     }
 
     /* =========================
-     * UTILS
+     * NONCE++
      * ========================= */
     private static void incrementNonce(byte[] nonce) {
         for (int i = 11; i >= 4; i--) {
@@ -94,32 +97,10 @@ public final class SecretStream {
     }
 
     private static byte[] longToBytes(long v) {
-        byte[] b = new byte[8];
+        byte[] out = new byte[8];
         for (int i = 0; i < 8; i++) {
-            b[i] = (byte) (v >>> (8 * i));
+            out[i] = (byte) (v >>> (8 * i));
         }
-        return b;
-    }
-}    
-    private boolean constantTimeCompare(byte[] a, byte[] b) {
-        if (a.length != b.length) {
-            return false;
-        }
-        int diff = 0;
-        for (int i = 0; i < a.length; i++) {
-            diff |= (a[i] ^ b[i]);
-        }
-        return diff == 0;
-    }
-    
-    private void store64_le(byte[] dst, int offset, long w) {
-        dst[offset]     = (byte) (w & 0xFF);
-        dst[offset + 1] = (byte) ((w >>> 8) & 0xFF);
-        dst[offset + 2] = (byte) ((w >>> 16) & 0xFF);
-        dst[offset + 3] = (byte) ((w >>> 24) & 0xFF);
-        dst[offset + 4] = (byte) ((w >>> 32) & 0xFF);
-        dst[offset + 5] = (byte) ((w >>> 40) & 0xFF);
-        dst[offset + 6] = (byte) ((w >>> 48) & 0xFF);
-        dst[offset + 7] = (byte) ((w >>> 56) & 0xFF);
+        return out;
     }
 }
