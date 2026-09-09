519432de6de508037eeccb3d5e1e340abfa3c7c3
Clouddark75 <84534855+Clouddark75@users.noreply.github.com>
2026-09-09 00:54:38 -0400

    Update JeazScans.kt

--- src/es/jeazscans/src/eu/kanade/tachiyomi/extension/es/jeazscans/JeazScans.kt
+++ src/es/jeazscans/src/eu/kanade/tachiyomi/extension/es/jeazscans/JeazScans.kt
@@ -11,11 +11,6 @@ import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.util.asJsoup
 import keiyoushi.utils.parseAs
 import keiyoushi.utils.tryParse
-import okhttp3.HttpUrl.Companion.toHttpUrl
-import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
 import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Calendar
